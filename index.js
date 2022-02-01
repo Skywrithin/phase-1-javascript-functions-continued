b1031fbec4dcc0ae700d1fc12ebe65acb7df58b6
Kaleb <kaleb.franken7@gmail.com>
2022-02-01 16:28:57 -0700

    commit tree 4e3ae6a193423db24b361795aabca35cdbdaca74

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+const saturdayFun = function(arg1 = 'roller-skate'){
+    return `This Saturday, I want to ${arg1}!`;
+}
+
+const wrapAdjective = function(flair){
+    return function(arg5 = 'special'){
+        return `You are ${flair}${arg5}${flair}!`;
+
+    }
+}
+
+
+/*
+function outer(greeting, msg = "It's a fine day to learn") {
+    return function (name, lang = "Python") {
+      return `${greeting}, ${name}! ${msg} ${lang}`;
+    };
+  }
+
+  function outer(greeting, msg = "It's a fine day to learn") {
+    const innerFunction = function (name, lang = "Python") {
+      return `${greeting}, ${name}! ${msg} ${lang}`;
+    };
+    return innerFunction;
+  }
+  
+  */
